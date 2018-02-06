425156f3adebfac5183bbddc27b0330a88b4c3f7
James da Silva <jamesodas@gmail.com>
2018-02-06 17:15:55 -0300

    [Initial Commit] Webpack e Babel com EC6 foram instalados e configurados.

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import teste from "./testando";
+
+teste();
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const path = require("path")
+
+module.exports = {
+    devtool: 'source-map',
+    entry: path.join(__dirname, 'src', 'index'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    module: {
+        loaders: [
+            {
+                test: /.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                  presets: ['es2015']
+                }
+            }
+        ]
+    }
+}
